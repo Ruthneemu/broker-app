32af13ef3f292008752b047bd21566a8d68a580d
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-09-13 12:24:07 +0300

    Update adminLogin.jsx

--- admin/src/adminLogin.jsx
+++ admin/src/adminLogin.jsx
@@ -23,7 +23,8 @@ const AdminLogin = () => {
     
     try {
       const { error } = await supabase.auth.resetPasswordForEmail(resetEmail, {
-        redirectTo: `${window.location.origin}/admin/reset-password`,
+        // Updated to use auth-handler route instead of reset-password
+        redirectTo: `${window.location.origin}/admin/auth-handler`,
       });
       
       if (error) {
@@ -94,7 +95,9 @@ const AdminLogin = () => {
           <div className="bg-red-50 border-l-4 border-red-500 p-4 mb-6 rounded">
             <div className="flex">
               <div className="flex-shrink-0">
-                <i className="fas fa-exclamation-circle text-red-500"></i>
+                <svg className="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                </svg>
               </div>
               <div className="ml-3">
                 <p className="text-sm text-red-700">{error}</p>
@@ -107,7 +110,9 @@ const AdminLogin = () => {
           <div className="bg-green-50 border-l-4 border-green-500 p-4 mb-6 rounded">
             <div className="flex">
               <div className="flex-shrink-0">
-                <i className="fas fa-check-circle text-green-500"></i>
+                <svg className="h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                </svg>
               </div>
               <div className="ml-3">
                 <p className="text-sm text-green-700">
@@ -235,4 +240,4 @@ const AdminLogin = () => {
   );
 };
 
-export default AdminLogin;            
+export default AdminLogin;
